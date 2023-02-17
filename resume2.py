2cffc2f4489bc549f7d2c1572d2aa307d2be3835
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-02-17 20:48:47 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -44,87 +44,87 @@ if st.button("Process"):
 	Skills_extracted=[num.strip('(') for num in Skills_extracted]
 
 #st.write("Skills",Skills_extracted)
-res={'skills_reqd_DataScientist':[],'skills_reqd_HR':[],'skills_reqd_sales':[]}
-for i in Skills_extracted:
-	if i in skills_reqd_DS:
-		res['skills_reqd_DataScientist'].append(str(i))
-	if i in skills_reqd_HR:
-		res['skills_reqd_HR'].append(str(i))
-	if i in skills_reqd_sales:
-		res['skills_reqd_sales'].append(str(i))
+	res={'skills_reqd_DataScientist':[],'skills_reqd_HR':[],'skills_reqd_sales':[]}
+	for i in Skills_extracted:
+		if i in skills_reqd_DS:
+			res['skills_reqd_DataScientist'].append(str(i))
+		if i in skills_reqd_HR:
+			res['skills_reqd_HR'].append(str(i))
+		if i in skills_reqd_sales:
+			res['skills_reqd_sales'].append(str(i))
    
 
 
 
-HR=0
-DS=0
-sales=0
-x=1
-c="He is Specialised in"
-b=""
-for i,j in res.items():
-    
-	if x==1:
-		DS=len(j)
-	if x==2:
-		HR=len(j)
-	if x==3:
-		sales=len(j)
-	x+=1
-if (HR>DS and HR>sales):
-	b="HR"
-	print('He is applicable for HR role')
-if (DS>HR and DS>sales):
-	b="DataScientist"
-	print('He is applicable for DataScientist role')
-if (sales>HR and sales>DS):
-	b="Sales"
-	print('He is applicable for Sales role')
+	HR=0
+	DS=0
+	sales=0
+	x=1
+	c="He is Specialised in"
+	b=""
+	for i,j in res.items():
 
-print(c+' '+b)
+		if x==1:
+			DS=len(j)
+		if x==2:
+			HR=len(j)
+		if x==3:
+			sales=len(j)
+		x+=1
+	if (HR>DS and HR>sales):
+		b="HR"
+		print('He is applicable for HR role')
+	if (DS>HR and DS>sales):
+		b="DataScientist"
+		print('He is applicable for DataScientist role')
+	if (sales>HR and sales>DS):
+		b="Sales"
+		print('He is applicable for Sales role')
+
+	print(c+' '+b)
 
 import pandas as pd
 
-sal_data=pd.read_csv(r"Sal_data.csv")
-sal_data.info()
+	sal_data=pd.read_csv(r"Sal_data.csv")
+	sal_data.info()
 
-sal_data=pd.DataFrame(sal_data)
-a=extract_for_YoE['total_exp']
+	sal_data=pd.DataFrame(sal_data)
+	a=extract_for_YoE['total_exp']
 
-st.write(a,b)
+	st.write(a,b)
 
-def final(a,b):
-	if a<=2 and b=="DataScientist":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "0-2")]
-		st.write(color_and_shape)
-		
-	if   2 < a <=4 and b=="DataScientist":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "2-4")]
-		st.write(color_and_shape)
-		
-	if 4< a <=10 and b=="DataScientist":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "5-10")]
-		st.write(color_and_shape)
-		
-	if a>=11 and b=="DataScientist":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "10+")]
-		st.write(color_and_shape)
-	if a<=2 and b=="HR":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "0-2")]
-		st.write(color_and_shape)
-     
-	if 2 < a <=4 and b=="HR":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "3-4")]
-		st.write(color_and_shape)
-    
-	if 4 < a <=10 and b=="HR":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "5-10")]
-		st.write(color_and_shape)
-	if a>=11 and b=="HR":
-		color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "10+")]
-		st.write(color_and_shape)
+	def final(a,b):
+		if a<=2 and b=="DataScientist":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "0-2")]
+			st.write(color_and_shape)
+
+		if   2 < a <=4 and b=="DataScientist":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "2-4")]
+			st.write(color_and_shape)
+
+		if 4< a <=10 and b=="DataScientist":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "5-10")]
+			st.write(color_and_shape)
+
+		if a>=11 and b=="DataScientist":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "DataScientist") & (sal_data['YoE'] == "10+")]
+			st.write(color_and_shape)
+		if a<=2 and b=="HR":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "0-2")]
+			st.write(color_and_shape)
+
+		if 2 < a <=4 and b=="HR":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "3-4")]
+			st.write(color_and_shape)
+
+		if 4 < a <=10 and b=="HR":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "5-10")]
+			st.write(color_and_shape)
+		if a>=11 and b=="HR":
+			color_and_shape = sal_data.loc[(sal_data['Job Role'] == "HR") & (sal_data['YoE'] == "10+")]
+			st.write(color_and_shape)
 
 	# else:
 	# 	st.write("bye")
-final(extract_for_YoE['total_exp'],b)
+	final(extract_for_YoE['total_exp'],b)
         
