e52dc43393fab2c44d9924598626c64ea226bc01
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-02-28 11:21:14 +0530

    Update resume2.py

--- resume2.py
+++ resume2.py
@@ -58,7 +58,7 @@ if st.button("Process"):
 			res['skills_reqd_HR'].append(str(i))
 		if i in skills_reqd_sales:
 			res['skills_reqd_sales'].append(str(i))
-	st.write(res)
+	st.write("The Skills that get Matched with our Keywords",res)
    
 
 	HR=0
@@ -152,7 +152,6 @@ if st.button("Process"):
 		
 	final(extract_for_YoE['total_exp'],b)
         
-	if st.button('Skills'):
-		st.write(res)
+	
 
 
